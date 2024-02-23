819b00adf76f9371b1ae3b5aca7548e4dde0f216
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2024-02-22 22:09:09 -0300

    Add files via upload

---
+++ Juros.kt
@@ -0,0 +1,81 @@
+// import kotlin.collections
+package juros
+import kotlin.math.pow
+
+class Juros (pQuantidade: Int, pComposto: Boolean, pPeriodo: Int) {
+    private var quantidade: Int = 0
+        set(value) {
+            field = value
+            pagamentos = Array(value){0}
+            pesos = Array(value){0.0}
+        }
+    private var composto: Boolean = false
+    private var periodo: Int = 30
+    private var pagamentos: Array<Int> = emptyArray()
+    private var pesos: Array<Double> = emptyArray()
+
+    init {
+        setValores(pQuantidade, pComposto, pPeriodo)
+    }
+
+    fun setValores(pQuantidade: Int, pComposto: Boolean, pPeriodo: Int) {
+        quantidade = pQuantidade
+        composto = pComposto
+        periodo = pPeriodo
+    }
+
+    fun setPagamento(indice: Int, valor: Int) {
+        pagamentos[indice] = valor
+    }
+
+    fun setPeso(indice: Int, valor: Double) {
+        pesos[indice] = valor
+    }
+
+    fun getPesoTotal() : Double {
+        var acumulador = 0.0
+        for(valor in pesos) acumulador += valor
+        return acumulador
+    }
+
+    fun jurosParaAcrescimo(juros: Double) : Double {
+        if(juros <= 0 || quantidade <= 0 || periodo <= 0) return 0.0
+        val pesoTotal = getPesoTotal()
+        if(pesoTotal <= 0) return 0.0
+        var acumulador = 0.0
+        var soZero = true
+
+        for(indice in 0..quantidade-1) {
+            if(pagamentos[indice] > 0 && pesos[indice] > 0) soZero = false
+            if(composto) {
+                acumulador += pesos[indice] / (1 + juros / 100).pow(pagamentos[indice] / periodo)
+            } else {
+                acumulador += pesos[indice] / (1 + juros / 100 * pagamentos[indice] / periodo)
+            }
+        }
+
+        if(soZero) return 0.0
+        return(pesoTotal / acumulador - 1) * 100
+    }
+
+    fun acrescimoParaJuros(acrescimo: Double, precisao: Int = 15, maxIteracoes: Int = 100, topJuros: Double = 50.0) : Double {
+        if(maxIteracoes < 1 || quantidade <= 0 || precisao < 1 || periodo <= 0 || acrescimo <=0 || topJuros <= 0) return 0.0
+        var minJuros = 0.0
+        var maxJuros = topJuros
+        var medJuros = 0.0
+        val minDiferenca = (0.1).pow(precisao)
+        val pesoTotal = getPesoTotal()
+        if(pesoTotal <= 0) return 0.0
+
+        for(indice in 1..maxIteracoes) {
+            medJuros = (minJuros + maxJuros) / 2
+            if((maxJuros - minJuros) < minDiferenca) break
+            if(jurosParaAcrescimo(medJuros) <= acrescimo) {
+                minJuros = medJuros
+            } else {
+                maxJuros = medJuros
+            }
+        }
+        return medJuros
+    }
+}
---
+++ Main.kt
@@ -0,0 +1,16 @@
+import juros.Juros
+
+fun main() {
+    val juros = Juros(3, true, 30)
+    juros.setPagamento(0, 30)
+    juros.setPagamento(1, 60)
+    juros.setPagamento(2, 90)
+    juros.setPeso(0, 1.0)
+    juros.setPeso(1, 1.0)
+    juros.setPeso(2, 1.0)
+    val acrescimoCalculado = juros.jurosParaAcrescimo(3.0)
+    val jurosCalculado = juros.acrescimoParaJuros(acrescimoCalculado)
+    println(juros.getPesoTotal())
+    println(acrescimoCalculado)
+    println(jurosCalculado)
+}

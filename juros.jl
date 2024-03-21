f579f75903a94adaf744ac0ed5a26cb2c63f8010
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2024-03-21 11:25:10 -0300

    Add files via upload
    testejuros em arquivo separado.

--- juros.jl
+++ juros.jl
@@ -1,5 +1,6 @@
 # Cálculo do juros, sendo que precisa de arrays pra isso
 # Versão 0.1: 20/08/2024: versão feita a partir de pesquisas no Google
+# Versão 0.2: 21/08/2024: versão com testejuros separado
 
 # Estrutura com os dados comuns (como parcelas)
 struct Juros
@@ -77,7 +78,7 @@ function acrescimoParaJuros(sJuros, acrescimo, precisao, maxIteracoes, maxJuros)
 end
 
 # testes
-juros = Juros(3, true, 30.0, [30.0, 60.0, 90.0], [1.0, 1.0, 1.0])
- println(getPesoTotal(juros))
- println(jurosParaAcrescimo(juros, 3.0))
- println(acrescimoParaJuros(juros, jurosParaAcrescimo(juros, 3.0), 15, 100, 50.0))
+# juros = Juros(3, true, 30.0, [30.0, 60.0, 90.0], [1.0, 1.0, 1.0])
+#  println(getPesoTotal(juros))
+#  println(jurosParaAcrescimo(juros, 3.0))
+#  println(acrescimoParaJuros(juros, jurosParaAcrescimo(juros, 3.0), 15, 100, 50.0))
---
+++ testejuros.jl
@@ -0,0 +1,6 @@
+include("juros.jl")
+
+juros = Juros(3, true, 30.0, [30.0, 60.0, 90.0], [1.0, 1.0, 1.0])
+ println(getPesoTotal(juros))
+ println(jurosParaAcrescimo(juros, 3.0))
+ println(acrescimoParaJuros(juros, jurosParaAcrescimo(juros, 3.0), 15, 100, 50.0))

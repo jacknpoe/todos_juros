f3e856bea7d31b6a62de782f403f72949b00b83f
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2024-04-01 16:24:49 -0300

    Add files via upload
    agora mostra com 15 casas decimais

--- testajuros.m
+++ testajuros.m
@@ -8,13 +8,13 @@ close all;
 % juros.Pagamentos = [30, 60, 90];
 % juros.Pesos = [1, 1, 1];
 
-% essa linha cria juros e faz o que as cinco linhas comentdas acima fazem
+% essa linha cria juros e faz o que as cinco linhas comentadas acima fazem
 juros = Juros(3, true, 30, [30,60,90], [1,1,1]);
 
 % testes
 pesoTotal = juros.getPesoTotal;
 acrescimo = juros.jurosParaAcrescimo(3.0);
 juroscalc = juros.acrescimoParaJuros(acrescimo, 15, 100, 50.0);
-fprintf("O peso total é = %d!\n", pesoTotal);
-fprintf("O acréscimo é = %d!\n", acrescimo);
-fprintf("Os juros são = %d!\n", juroscalc);
+fprintf("O peso total é = %18.15d!\n", pesoTotal);
+fprintf("O acréscimo é = %18.15d!\n", acrescimo);
+fprintf("Os juros são = %18.15d!\n", juroscalc);

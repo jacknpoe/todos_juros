e9dd06d1a64bfaad17567b13940284af32129335
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2024-04-14 18:26:03 -0300

    Add files via upload
    alterado texto da etiqueta

--- main.dart
+++ main.dart
@@ -131,7 +131,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'Os resultados são:',
+              'Os resultados são (clique no botão para gerar):',
             ),
             Text(
               _texto,

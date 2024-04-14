b8e58387469645e216c540f79a2ded18a946f8ba
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2024-04-14 17:20:53 -0300

    Add files via upload
    retirados alguns comentários da documentação do wizard

--- main.dart
+++ main.dart
@@ -128,22 +128,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -160,7 +145,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _botaoMostrar,
         tooltip: 'Mostra os resultados',
         child: const Icon(Icons.play_arrow),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), 
     );
   }
 }

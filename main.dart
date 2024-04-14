10d50cabf94aea809f85b146b4fde77a22efae84
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2024-04-14 18:23:45 -0300

    Add files via upload
    trocado o título e retirado um comentário do wizard

--- main.dart
+++ main.dart
@@ -75,16 +75,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Calcula Juros',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Teste de Cálculo de Juros'),
     );
   }
 }

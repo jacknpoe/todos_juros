9d11b5d234a8df63e05eda4a2f5468183342343e
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2024-04-25 07:27:53 -0300

    Update Juros.gd
    comentário v 0.2

--- Juros.gd
+++ Juros.gd
@@ -1,5 +1,6 @@
 # Cálculo do juros, sendo que precisa de arrays pra isso
 # Versão 0.1: 08/04/2024: versão feita sem muito conhecimento de Godot
+# Versão 0.2:    04/2024: trocada avaliação soZero por acumulador == 0
 
 extends Node2D
 

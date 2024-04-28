a7a1757c5a318120f2cc73341ae982c511cfcf2e
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2024-04-28 18:58:47 -0300

    Add files via upload
    alterados todos os números ponto flutuante para double para mais casas depois da vírgula

--- juros.lisp
+++ juros.lisp
@@ -5,11 +5,11 @@
 ;; Variáveis
 (defvar Quantidade 3)
 (defvar Composto 1)
-(defvar Periodo 30.0)
-(defvar Pagamentos '(30.0 60.0 90.0))
-(defvar Pesos '(1.0 1.0 1.0))
-(defvar pesoTotal 0.0)
-(defvar medJuros 0.0)
+(defvar Periodo 30.0d0)
+(defvar Pagamentos '(30.0d0 60.0d0 90.0d0))
+(defvar Pesos '(1.0d0 1.0d0 1.0d0))
+(defvar pesoTotal 0.0d0)
+(defvar medJuros 0.0d0)
 
 ;; calcula a somatória de Pesos[]
 (defun getPesoTotal()
@@ -30,24 +30,24 @@
   (setq pesoTotal (getPesoTotal))
   (if (<= pesoTotal 0.0) (return-from jurosParaAcrescimo 0.0))
   (if (= Composto 1)
-      (* (- (/ pesoTotal (_jurosCompostos(- Quantidade 1) juros)) 1) 100)
-      (* (- (/ pesoTotal (_jurosSimples(- Quantidade 1) juros)) 1) 100)
+      (* (- (/ pesoTotal (_jurosCompostos(- Quantidade 1) juros)) 1.0d0) 10.0d00)
+      (* (- (/ pesoTotal (_jurosSimples(- Quantidade 1) juros)) 1.0d0) 100.0d0)
   )
 )
 
 ;; calcula a soma do amortecimento de todas as parcelas para juros compostos
 (defun _jurosCompostos(valor juros)
   (if (= valor 0)
-    (/ (nth 0 Pesos) (expt (+ 1.0 (/ juros 100.0)) (/ (nth 0 Pagamentos) Periodo)))
-    (+ (/ (nth valor Pesos) (expt (+ 1.0 (/ juros 100.0)) (/ (nth valor Pagamentos) Periodo))) (_jurosCompostos (- valor 1) juros))
+    (/ (nth 0 Pesos) (expt (+ 1.0d0 (/ juros 100.0d0)) (/ (nth 0 Pagamentos) Periodo)))
+    (+ (/ (nth valor Pesos) (expt (+ 1.0d0 (/ juros 100.0d0)) (/ (nth valor Pagamentos) Periodo))) (_jurosCompostos (- valor 1) juros))
   )
 )
 
 ;; calcula a soma do amortecimento de todas as parcelas para juros simples
 (defun _jurosSimples(valor juros)
   (if (= valor 0)
-    (/ (nth 0 Pesos) (+ 1.0 (* (/ juros 100.0) (/ (nth 0 Pagamentos) Periodo))))
-    (+ (/ (nth valor Pesos) (+ 1.0 (* (/ juros 100.0) (/ (nth valor Pagamentos) Periodo)))) (_jurosSimples (- valor 1) juros))
+    (/ (nth 0 Pesos) (+ 1.0d0 (* (/ juros 100.0d0) (/ (nth 0 Pagamentos) Periodo))))
+    (+ (/ (nth valor Pesos) (+ 1.0d0 (* (/ juros 100.0d0) (/ (nth valor Pagamentos) Periodo)))) (_jurosSimples (- valor 1) juros))
   )
 )
 
@@ -56,12 +56,12 @@
   (if (or (or (or (or (or (<= acrescimo 0.0) (<= Quantidade 0)) (<= Periodo 0.0)) (< maxIteracoes 1)) (< precisao 1)) (<= maxJuros 0.0)) (return-from acrescimoParaJuros 0.0))
   (setq pesoTotal (getPesoTotal))
   (if (<= pesoTotal 0.0) (return-from acrescimoParaJuros 0.0))
-  (_acrescimoParaJuros acrescimo (expt 0.1 precisao) maxIteracoes 0.0 maxJuros)
+  (_acrescimoParaJuros acrescimo (expt 0.1d0 precisao) maxIteracoes 0.0d0 maxJuros)
 )
 
 ;; função recursiva no lugar de um for que realmente calcula o acrésimo
 (defun _acrescimoParaJuros(acrescimo minDiferenca iteracaoatual minJuros maxJuros)
-  (setq medJuros (/ (+ minJuros maxJuros) 2.0))
+  (setq medJuros (/ (+ minJuros maxJuros) 2.0d0))
   (if (= iteracaoatual 1) (return-from _acrescimoParaJuros medJuros ))
   (if (< (- maxJuros minJuros) minDiferenca) (return-from _acrescimoParaJuros medJuros))
   (if (< (jurosParaAcrescimo medJuros) acrescimo)
@@ -75,8 +75,7 @@
 (write (getPesoTotal))
 (terpri)
 (princ "Acrescimo = ")
-(write (jurosParaAcrescimo 3.0))
+(write (jurosParaAcrescimo 3.0d0))
 (terpri)
 (princ "Juros = ")
-(write (acrescimoParaJuros (jurosParaAcrescimo 3.0) 18 100 50.0))
-
+(write (acrescimoParaJuros (jurosParaAcrescimo 3.0d0) 18 100 50.0d0))

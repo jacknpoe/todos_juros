b07cceac89b4b4feb3568d4589ce40c21e406559
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2024-05-04 11:52:13 -0300

    Add files via upload
    + Haskell

---
+++ juros.hs
@@ -0,0 +1,53 @@
+-- Cálculo do juros, sendo que precisa de arrays pra isso
+-- Versão 0.1: 04/05/2024: versão feita sem muito conhecimento de Haskell
+{-# OPTIONS -fdefer-type-errors #-}
+import Data.List
+
+-- estrutura básica para simplificar as chamadas
+composto = True
+periodo = 30.0
+pagamentos = [30.0, 60.0, 90.0]
+pesos = [1.0, 1.0, 1.0]
+
+-- calcula a somatória de Pesos[]
+getPesoTotal =
+   rGetPesoTotal pesos
+
+-- função recursiva que realmente calcula a somatória de pesos[]
+rGetPesoTotal (h:t)
+   | t == [] = h
+   | otherwise = h + rGetPesoTotal t
+
+-- calcula o acréscimo a partir dos juros e dados comuns (como parcelas)
+jurosParaAcrescimo juros
+  | composto = (getPesoTotal / (rJurosCompostos pagamentos pesos juros) - 1.0) * 100.0
+  | otherwise = (getPesoTotal / (rJurosSimples pagamentos pesos juros) - 1.0) * 100.0
+
+-- calcula a soma do amortecimento de todas as parcelas para juros compostos
+rJurosCompostos (pagH:pagT) (pesH:pesT) juros
+  | pesT == [] = pesH / (1.0 + juros / 100.0) ** (pagH / periodo)
+  | otherwise = pesH / (1.0 + juros / 100.0) ** (pagH / periodo) + rJurosCompostos pagT pesT juros
+
+-- calcula a soma do amortecimento de todas as parcelas para juros simples
+rJurosSimples (pagH:pagT) (pesH:pesT) juros
+  | pesT == [] = pesH / (1.0 + juros / 100.0 * pagH / periodo)
+  | otherwise = pesH / (1.0 + juros / 100.0 * pagH / periodo) + rJurosSimples pagT pesT juros
+
+-- calcula os juros a partir do acréscimo e dados comuns (como parcelas)
+acrescimoParaJuros acrescimo precisao maxIteracoes maxJuros =
+  rAcrescimoParaJuros acrescimo (0.1 ** precisao) maxIteracoes 0.0 maxJuros (maxJuros / 2.0)
+
+-- função recursiva no lugar de um for que realmente calcula o acréscimo
+rAcrescimoParaJuros acrescimo minDiferenca iteracaoAtual minJuros maxJuros medJuros
+  | iteracaoAtual == 0 = medJuros
+  | (maxJuros - minJuros) < minDiferenca = medJuros
+  | jurosParaAcrescimo medJuros < acrescimo = rAcrescimoParaJuros acrescimo minDiferenca (iteracaoAtual - 1) medJuros maxJuros ((medJuros + maxJuros) / 2.0)
+  | otherwise = rAcrescimoParaJuros acrescimo minDiferenca (iteracaoAtual - 1) minJuros medJuros ((minJuros + medJuros) / 2.0)
+
+main = do
+  putStr "Peso total = "
+  putStrLn (show(getPesoTotal))
+  putStr "Juros = "
+  putStrLn (show(jurosParaAcrescimo 3.0))
+  putStr "Acréscimo = "
+  putStrLn (show( acrescimoParaJuros (jurosParaAcrescimo 3.0) 15 100 50.0))  

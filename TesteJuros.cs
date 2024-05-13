bf6f6f681e19e16635988c5e6bda0d998b37068b
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2024-05-13 17:07:32 -0300

    Add files via upload
    colocado peso total e etiquetas nas linhas

--- TesteJuros.cs
+++ TesteJuros.cs
@@ -14,10 +14,15 @@ class TesteJuros {
         juros.Pagamentos[1] = 60.0;
         juros.Pagamentos[2] = 90.0;
 
+        double pesoTotal = juros.getPesoTotal();
         double acrescimoCalculado = juros.jurosParaAcrescimo(3.0);
         double jurosCalculado = juros.acrescimoParaJuros(acrescimoCalculado);
 
+        System.Console.Write("Peso total = ");
+        System.Console.WriteLine(pesoTotal);
+        System.Console.Write("Acréscimo = ");
         System.Console.WriteLine(acrescimoCalculado);
+        System.Console.Write("Juros = ");
         System.Console.WriteLine(jurosCalculado);
         System.Console.ReadLine();
     }

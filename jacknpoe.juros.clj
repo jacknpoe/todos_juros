c5b242f57f9fd011de15c2df9899d39d7e8cb732
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2024-05-17 17:57:04 -0300

    Add files via upload
    dec no lugar de - 1

--- jacknpoe.juros.clj
+++ jacknpoe.juros.clj
@@ -2,6 +2,7 @@
 ;; Versão 0.1: 16/05/2024: versão feita sem muito conhecimento de Clojure: completa
 ;;        0.2: 17/05/2024: versão com os ifs que retornam 0.0 na mesma linha (+ getPesoTotal)
 ;;        0.3: 17/05/2024: versão com vetores no lugar de listas
+;;        0.4: 17/05/2024: dec no lugar de - 1
 
 ;; namespace
 (ns jacknpoe.juros (:gen-class))
@@ -19,20 +20,20 @@
 (defn rGetPesoTotal [valor]
   (if (= valor 0)
     (nth Pesos 0) 
-    (+ (nth Pesos valor) (rGetPesoTotal (- valor 1)))
+    (+ (nth Pesos valor) (rGetPesoTotal (dec valor)))
   )
 )
 
 ;; calcula a somatória de todos os pesos
 (defn getPesoTotal []
-  (rGetPesoTotal(- Quantidade 1))
+  (rGetPesoTotal(dec Quantidade))
 )
 
 ;; calcula a soma do amortecimento de todas as parcelas para juros compostos
 (defn rJurosCompostos [valor juros]
   (if (= valor 0)
     (/ (nth Pesos 0) (Math/pow (+ 1.0 (/ juros 100.0)) (/ (nth Pagamentos 0) Periodo)))
-    (+ (/ (nth Pesos valor) (Math/pow (+ 1.0 (/ juros 100.0)) (/ (nth Pagamentos valor) Periodo))) (rJurosCompostos (- valor 1) juros))
+    (+ (/ (nth Pesos valor) (Math/pow (+ 1.0 (/ juros 100.0)) (/ (nth Pagamentos valor) Periodo))) (rJurosCompostos (dec valor) juros))
   )
 )
 
@@ -40,7 +41,7 @@
 (defn rJurosSimples [valor juros]
   (if (= valor 0)
     (/ (nth Pesos 0) (+ 1.0 (* (/ juros 100.0) (/ (nth Pagamentos 0) Periodo))))
-    (+ (/ (nth Pesos valor) (+ 1.0 (* (/ juros 100.0) (/ (nth Pagamentos valor) Periodo)))) (rJurosSimples (- valor 1) juros))
+    (+ (/ (nth Pesos valor) (+ 1.0 (* (/ juros 100.0) (/ (nth Pagamentos valor) Periodo)))) (rJurosSimples (dec valor) juros))
   )
 )
 
@@ -50,8 +51,8 @@
   (if (or (<= juros 0.0) (< Quantidade 1) (<= Periodo 0.0) (<= (getPesoTotal) 0.0))
     0.0
     (if Composto
-      (* (- (/ (getPesoTotal) (rJurosCompostos (- Quantidade 1) juros)) 1.0) 100.0)
-      (* (- (/ (getPesoTotal) (rJurosSimples (- Quantidade 1) juros)) 1.0) 100.0)
+      (* (- (/ (getPesoTotal) (rJurosCompostos (dec Quantidade) juros)) 1.0) 100.0)
+      (* (- (/ (getPesoTotal) (rJurosSimples (dec Quantidade) juros)) 1.0) 100.0)
     )
   )
 )
@@ -61,8 +62,8 @@
   (if (or (= iteracaoAtual 0) (< (- maxJuros minJuros) minDiferenca))
     medJuros
     (if (< (jurosParaAcrescimo medJuros) acrescimo)
-      (rAcrescimoParaJuros acrescimo minDiferenca (- iteracaoAtual 1) medJuros maxJuros (/ (+ medJuros maxJuros) 2.0))
-      (rAcrescimoParaJuros acrescimo minDiferenca (- iteracaoAtual 1) minJuros medJuros (/ (+ minJuros medJuros) 2.0))
+      (rAcrescimoParaJuros acrescimo minDiferenca (dec iteracaoAtual) medJuros maxJuros (/ (+ medJuros maxJuros) 2.0))
+      (rAcrescimoParaJuros acrescimo minDiferenca (dec iteracaoAtual) minJuros medJuros (/ (+ minJuros medJuros) 2.0))
     )
   )  
 )

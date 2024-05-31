a255b809f70ec485fc7c8ffeda2318b2cef835cc
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2024-05-31 18:12:05 -0400

    Add files via upload
    + SML

--- juros.ml
+++ juros.ml
@@ -1,53 +1,58 @@
-(** Cálculo dos juros, sendo que precisa de arrays para isso *)
-(** Versão 0.1: 30/05/2024: versão feita sem muito conhecimento de OCaml *)
+(* Cálculo do juros, sendo que precisa de listas pra isso
+   Versão 0.1: 31/05/2024: completo, mas sem saber muito sobre SML *)
 
-(** valores para simplificar as chamadas *)
-let quantidade : int = 3
-let composto = true
-let periodo = 30.0
-let pagamentos = [| 30.0; 60.0; 90.0 |]
-let pesos = [| 1.0; 1.0; 1.0|]
+load "Math";
+load "Real";
 
-(** função recursiva que calcula a somatória de Pesos[] *)
-let rec rGetPesoTotal indice =
-  if indice = 0 then pesos.(0)
-  else pesos.(indice) +. rGetPesoTotal(indice-1);;
+(* variáveis básicas para simplificar as chamadas *)
+val quantidade = 3;
+val composto = true;
+val periodo = 30.0;
+val pagamentos = [30.0, 60.0, 90.0];
+val pesos = [1.0, 1.0, 1.0];
 
-(** calcula a somatória de Pesos[] *)
-let getPesoTotal = rGetPesoTotal(quantidade-1);;
+(* função recursiva que realmente calcula a somatória de Pesos[] *)
+fun rGetPesoTotal indice = if indice = 0 then List.nth(pesos, 0) else rGetPesoTotal (indice - 1) + List.nth(pesos, indice) ;
+    
+(* perfume que calcula a somatória de Pesos[] *)
+fun getPesoTotal () = rGetPesoTotal (quantidade - 1);
 
-(** função recursiva que calcula o amortecimento das parcelas em juros compostos *)
-let rec rJurosCompostos indice juros =
-  if indice = 0 then pesos.(0) /. (1.0 +. juros /. 100.0) ** (pagamentos.(0) /. periodo)
-  else pesos.(indice) /. (1.0 +. juros /. 100.0) ** (pagamentos.(indice) /. periodo) +. rJurosCompostos (indice-1) juros ;;
+(* calcula a soma do amortecimento de todas as parcelas para juros compostos *)
+fun rJurosCompostos indice juros =
+    if indice = 0 then List.nth( pesos, 0) / Math.pow(1.0 + juros / 100.0, List.nth(pagamentos, 0) / periodo)
+    else rJurosCompostos (indice - 1) juros + List.nth(pesos, indice) / Math.pow(1.0 + juros / 100.0, List.nth(pagamentos, indice) / periodo);
 
-(** função recursiva que calcula o amortecimento das parcelas em juros simples *)
-let rec rJurosSimples indice juros =
-  if indice = 0 then pesos.(0) /. (1.0 +. juros /. 100.0 *. pagamentos.(0) /. periodo)
-  else pesos.(indice) /. (1.0 +. juros /. 100.0 *. pagamentos.(indice) /. periodo) +. rJurosSimples (indice-1) juros ;;
+(* calcula a soma do amortecimento de todas as parcelas para juros simples *)
+fun rJurosSimples indice juros =
+    if indice = 0 then List.nth(pesos, 0) / (1.0 + juros / 100.0 * List.nth(pagamentos, 0) / periodo)
+    else rJurosSimples (indice - 1) juros + List.nth(pesos, indice) / (1.0 + juros / 100.0 * List.nth(pagamentos, indice) / periodo);
 
-(** calcula o acréscimo a partir dos juros e das parcelas *)
-let jurosParaAcrescimo juros =
-  if composto then (getPesoTotal /. (rJurosCompostos (quantidade-1) juros) -. 1.0) *. 100.0 
-  else (getPesoTotal /. (rJurosSimples (quantidade-1) juros) -. 1.0) *. 100.0  ;;
+(* calcula o acréscimo a partir dos juros e dados comuns (como parcelas) *)
+fun jurosParaAcrescimo juros =
+    if composto then (getPesoTotal() / rJurosCompostos (quantidade - 1) juros - 1.0) * 100.0
+    else (getPesoTotal() / rJurosSimples (quantidade - 1) juros - 1.0) * 100.0;
 
-(** função recursiva no lugar de um for que realmente calcula o acréscimo *)
-let rec rAcrescimoParaJuros acrescimo minDiferenca iteracaoAtual minJuros maxJuros medJuros =
-  if iteracaoAtual = 0 || (maxJuros -. minJuros) < minDiferenca then medJuros
-  else
-    if jurosParaAcrescimo medJuros < acrescimo then rAcrescimoParaJuros acrescimo minDiferenca (iteracaoAtual-1) medJuros maxJuros ((medJuros +. maxJuros) /. 2.0)
-    else rAcrescimoParaJuros acrescimo minDiferenca (iteracaoAtual-1) minJuros medJuros ((minJuros +. medJuros) /. 2.0);;
+(* função recursiva no lugar de um for que realmente calcula o acréscimo *)
+fun rAcrescimoParaJuros acrescimo minDiferenca iteracaoAtual minJuros maxJuros medJuros =
+    if (iteracaoAtual = 0) orelse ((maxJuros - minJuros) < minDiferenca)
+    then medJuros
+    else if (jurosParaAcrescimo medJuros) < acrescimo
+         then rAcrescimoParaJuros acrescimo minDiferenca (iteracaoAtual - 1) medJuros maxJuros ((medJuros + maxJuros) / 2.0)
+         else rAcrescimoParaJuros acrescimo minDiferenca (iteracaoAtual - 1) minJuros medJuros ((minJuros + medJuros) / 2.0);
 
-(** calcula os juros a partir do acréscimo e das parcelas *)
-let acrescimoParaJuros acrescimo precisao maxIteracoes maxJuros =
-  rAcrescimoParaJuros acrescimo (0.1 ** (float precisao)) maxIteracoes 0.0 maxJuros (maxJuros /. 2.0);;
+(* calcula os juros a partir do acréscimo e dados comuns (como parcelas) *)
+fun acrescimoParaJuros acrescimo precisao maxIteracoes maxJuros =
+    rAcrescimoParaJuros acrescimo (Math.pow(0.1, precisao)) maxIteracoes 0.0 maxJuros (maxJuros / 2.0);
 
-(** faz os cálculos para testes *)
-let pesoTotal = getPesoTotal;;
-let acrescimoCalculado = jurosParaAcrescimo 3.0;;
-let jurosCalculado = acrescimoParaJuros acrescimoCalculado 15 100 50.0;;
+(* faz os cálculos para testes *)
+val pesoTotal = getPesoTotal();
+val acrescimoCalculado = jurosParaAcrescimo 3.0;
+val jurosCalculado = acrescimoParaJuros acrescimoCalculado 15.0 100 50.0;
 
-(** imprime os resultados *)
-Printf.printf "Peso total = %.15f\n" pesoTotal;
-Printf.printf "Acréscimo = %.15f\n" acrescimoCalculado;
-Printf.printf "Juros = %.15f\n" jurosCalculado;
+(* imprime as variáveis calculadas *)
+print "Peso total = ";
+print (Real.toString(pesoTotal));
+print "Acrescimo = ";
+print (Real.toString(acrescimoCalculado));
+print "Juros = ";
+print (Real.toString(jurosCalculado));

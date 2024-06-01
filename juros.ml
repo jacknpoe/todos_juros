033656a9e228b25e8e28e6742b864a6b06f54418
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2024-06-01 12:34:31 -0300

    Update juros.ml
    juntadas algumas linhas

--- juros.ml
+++ juros.ml
@@ -9,9 +9,7 @@ let pagamentos = [| 30.0; 60.0; 90.0 |]
 let pesos = [| 1.0; 1.0; 1.0|]
 
 (** função recursiva que calcula a somatória de Pesos[] *)
-let rec rGetPesoTotal indice =
-  if indice = 0 then pesos.(0)
-  else pesos.(indice) +. rGetPesoTotal(indice-1);;
+let rec rGetPesoTotal indice = if indice = 0 then pesos.(0) else pesos.(indice) +. rGetPesoTotal(indice-1);;
 
 (** calcula a somatória de Pesos[] *)
 let getPesoTotal = rGetPesoTotal(quantidade-1);;
@@ -33,8 +31,7 @@ let jurosParaAcrescimo juros =
 
 (** função recursiva no lugar de um for que realmente calcula o acréscimo *)
 let rec rAcrescimoParaJuros acrescimo minDiferenca iteracaoAtual minJuros maxJuros medJuros =
-  if iteracaoAtual = 0 || (maxJuros -. minJuros) < minDiferenca then medJuros
-  else
+  if iteracaoAtual = 0 || (maxJuros -. minJuros) < minDiferenca then medJuros else
     if jurosParaAcrescimo medJuros < acrescimo then rAcrescimoParaJuros acrescimo minDiferenca (iteracaoAtual-1) medJuros maxJuros ((medJuros +. maxJuros) /. 2.0)
     else rAcrescimoParaJuros acrescimo minDiferenca (iteracaoAtual-1) minJuros medJuros ((minJuros +. medJuros) /. 2.0);;
 

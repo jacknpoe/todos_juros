68971b5f9b8b34e01456d4f7526d145bb231e9f2
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2024-06-06 16:37:23 -0400

    Add files via upload
    adicionados comentários, legendas e pesoTotal

--- Juros.java
+++ Juros.java
@@ -1,24 +1,31 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- * Versão 0.2:    04/2024: trocada avaliação soZero por acumulador == 0
  */
+
+// Cálculo do juros, sendo que precisa de arrays pra isso
+// Versão 0.1: 27/02/2024: publicação inicial, sem comentários nem legendas
+//        0.2: 20/03/2004: pagamentos e períodos de int para double
+//        0.3: 04/04/2004: trocado de avaliar soZero para se acumulador <= 0.0
+//        0.4: 06/06/2024: com comentários e legendas
 
 package jacknpoe.testejuros;
 
-import java.lang.Math;
+import java.lang.Math;	// para Math.pow() 
 
 /**
  *
  * @author Ricardo
  */
+// classe com os atributos para simplificação das chamadas
 public class Juros {
     private int Quantidade;
     public boolean Composto;
     public double Periodo;
     public double Pagamentos[];
     public double Pesos[];
-    
+   
+    // construtor, que inicializa Quantidade, Composto e Periodo
     public Juros(int quantidade, boolean composto, double periodo) {
         this.Quantidade = quantidade;
         this.Composto = composto;
@@ -27,20 +34,24 @@ public class Juros {
         this.Pesos = new double[quantidade];
     }
 
+    // get de Quantidade
     public int getQuantidade() { return Quantidade; }
 
+    // set de Quantidade (altera os tamanhos dos arrays Pagamentos[] e Pesos[]
     public void setQuantidade(int valor) {
         this.Quantidade = valor;
         this.Pagamentos = new double[valor];
         this.Pesos = new double[valor];
     }
     
+    // calcula a somatória de Pesos[]
     public double getPesoTotal() {
         double acumulador = 0.0;
         for(int indice = 0; indice < this.Quantidade; indice++) acumulador += this.Pesos[indice];
         return acumulador;
     }
     
+    // calcula o acréscimo a partir dos juros e das parcelas
     public double jurosParaAcrescimo(double juros){
         if(juros <= 0 || this.Quantidade <= 0 || this.Periodo <= 0.0) return 0.0;
         double pesoTotal = this.getPesoTotal();
@@ -62,6 +73,7 @@ public class Juros {
         return (pesoTotal / acumulador - 1) * 100;
     }
 
+    // calcula os juros a partir do acréscimo e das parcelas
     public double acrescimoParaJuros(double acrescimo, int precisao, int maxIteracoes, double maxJuros) {
         if(maxIteracoes < 1 || this.Quantidade <= 0 || precisao < 1 || this.Periodo <= 0.0 || acrescimo <= 0 || maxJuros <= 0) return 0;
         double minJuros = 0.0, medJuros = 0.0, minDiferenca = Math.pow(0.1, precisao), pesoTotal = this.getPesoTotal();

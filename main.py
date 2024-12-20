a43e8fe7326d4b53d958da508b9c611e94510610
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2024-12-20 13:19:43 -0300

    Add files via upload
    Acentuação em "Acréscimo".

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+# coding: latin1
+
 # Calcula o acréscimo a partir dos juros e os juros a partir do acréscimo
 # Versões: 0.1: 24/08/2023: Primeiros métodos e propriedades, para testes
 #          0.2: 25/08/2023: Cálculos e testes com prints
 #          0.3: 10/07/2024: melhorados alguns ifs (depois de total) e números ponto flutuantes com .0
 #          0.4: 12/07/2024: corrigido o soZero, que não deveria existir
 #          0.5: 16/07/2024: corrigidos alguns valores padrão que não estavam em ponto flutuante
+#          0.6: 20/12/2024: com acentuação (Latin1)
+
 import Juros
 
 juros = Juros.Juros(3, True, 30.0)
@@ -11,5 +15,5 @@ juros.setpagamentos(",", "30.0,60.0,90.0")
 juros.setpesos(",", "1.0,1.0,1.0")
 
 print("Peso total = " + str(juros.getpesototal()))
-print("Acrescimo = " + str(juros.jurosparaacrescimo(3.0)))
+print("Acréscimo = " + str(juros.jurosparaacrescimo(3.0)))
 print("Juros = " + str(juros.acrescimoparajuros(juros.jurosparaacrescimo(3.0), 18)))

af82a020a1a3522971753517f906f03943b90f0e
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2025-07-04 18:55:23 -0400

    Add files via upload
    + Bash

---
+++ juros.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# Cálculo dos juros, sendo que precisa de parcelas pra isso
+# Versão 0.1: 04/07/2025: versão feita sem muito conhecimento de Bash
+# ATENÇÃO: como os cálculos de ponto flutuante são feitos usando o comando "bc -l", que é lento no Git Bash,
+#          nesse ambiente os resultados podem demorar demais, principalmente para muitas parcelas. Trocando o
+#          comando por "awk", que é relativamente mais rápido, o número de casas decimais será apenas cinco.
+
+# variáveis globais para simplificar as chamadas
+Quantidade=3
+Periodo="30.0"
+Composto=1  # 1 = TRUE
+declare -a Pagamentos
+declare -a Pesos
+
+# inicializa os arrays Pagamentos e Pesos
+for ((indice=0; indice<$Quantidade; indice++ )) do
+    Pagamentos[indice]=$(echo "($indice + 1) * $Periodo" | bc -l)
+    Pesos[indice]="1.0"
+done
+
+# calcula a somatória do array Pesos
+getPesoTotal() {
+    local acumulador="0.0"
+    for ((indice=0; indice<$Quantidade; indice++ )) do
+        acumulador=$(echo "$acumulador + ${Pesos[$indice]}" | bc -l)
+    done
+    echo "$acumulador"
+    return 0
+}
+
+# calcula o acréscimo a partir dos juros e parcelas
+jurosParaAcrescimo() {
+    local juros=$1
+    local pesoTotal=$(getPesoTotal)
+    if (( $(echo "$Quantidade < 1 || $Periodo <= 0.0 || $pesoTotal <= 0.0 || $juros <= 0.0" | bc -l) )); then
+        echo "0.0"
+        return 1
+    fi
+
+    local acumulador="0.0"
+    for ((indice=0; indice<$Quantidade; indice++ )) do
+        if [ "$Composto" -eq 1 ]; then
+            acumulador=$(echo "$acumulador + ${Pesos[$indice]} / e(${Pagamentos[$indice]} / $Periodo * l(1.0 + $juros / 100.0))" | bc -l)
+        else
+            acumulador=$(echo "$acumulador + ${Pesos[$indice]} / (1.0 + $juros / 100.0 * ${Pagamentos[$indice]} / $Periodo)" | bc -l)
+        fi
+    done
+
+    if (( $(echo "$acumulador <= 0.0" | bc -l) )); then
+        echo "0.0"
+        return 1
+    fi
+
+    echo "($pesoTotal / $acumulador - 1.0) * 100.0" | bc -l
+    return 0
+}
+
+# calcula os juros a partir do acréscimo e parcelas
+acrescimoParaJuros() {
+    local acrescimo=$1
+    local precisao=$2
+    local maxIteracoes=$3
+    local maxJuros=$4
+    local pesoTotal=$(getPesoTotal)
+    if (( $(echo "$Quantidade < 1 || $Periodo <= 0.0 || $pesoTotal <= 0.0 || $acrescimo <= 0.0 || $precisao < 1 || $maxIteracoes < 1 || $maxJuros <= 0.0" | bc -l) )); then
+        echo "0.0"
+        return 1
+    fi
+
+    local resultado
+    local minJuros="0.0"
+    local medJuros=$(echo "$maxJuros / 2.0" | bc -l)
+    local minDiferenca=$(echo "e($precisao * l(0.1))" | bc -l)
+
+    for ((indice=0; indice<$maxIteracoes; indice++ )) do
+        if (( $(echo "$maxJuros - $minJuros < $minDiferenca" | bc -l) )); then
+            echo "$medJuros"
+            return 0
+        fi
+
+        resultado=$(jurosParaAcrescimo "$medJuros")
+        if (( $(echo "$resultado < $acrescimo" | bc -l) )); then
+            minJuros="$medJuros"
+        else
+            maxJuros="$medJuros"
+        fi
+
+        medJuros=$(echo "($minJuros + $maxJuros) / 2.0" | bc -l)
+    done
+
+    echo "$medJuros"
+    return 0
+}
+
+# calcula e guarda os retornos das funções
+pesoTotal=$(getPesoTotal)
+acrescimoCalculado=$(jurosParaAcrescimo "3.0")
+jurosCalculado=$(acrescimoParaJuros "$acrescimoCalculado" 20 75 "50.0")
+
+# imprime os resultados
+echo "Peso total = $pesoTotal"
+echo "Acréscimo = $acrescimoCalculado"
+echo "Juros = $jurosCalculado"

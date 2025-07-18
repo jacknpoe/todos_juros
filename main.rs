b2b7d8fa707736ebca779873421e2d7e4aa3285f
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2025-07-18 18:23:46 -0300

    Add files via upload
    + preenchiemento dinâmico para mais de três parcelas

--- main.rs
+++ main.rs
@@ -5,11 +5,24 @@ mod juros;
 ///         0.3 19/03/2024: com chamadas por referência em pagamentos e pesos
 ///         0.4 04/04/2024: trocada avaliação soZero por acumulador == 0
 ///         0.4 11/04/2024: melhorados comentários e colocadas legendas nos testes
+///         0.5 18/07/2025: para mais de três parcelas, com preenchimento dinâmico
+
 fn main() {
     // testes das funções
-    let pagamentos: Vec<f64> = vec![30.0, 60.0, 90.0];
-    let pesos: Vec<f64> = vec![1.0, 1.0, 1.0];
-    let juros = juros::Juros::novo(3, true, 30.0, &pagamentos, &pesos);
+    let quantidade: usize = 3;
+    let composto: bool = true;
+    let periodo: f64 = 30.0;
+    let mut pagamentos: Vec<f64> = vec![0.0; quantidade];
+    let mut pesos: Vec<f64> = vec![0.0; quantidade];
+
+    let mut prox_pag: f64 = periodo;
+    for indice in 0..quantidade {
+        pagamentos[indice] = prox_pag;
+        pesos[indice] = 1.0;
+        prox_pag = prox_pag + periodo;
+    }
+
+    let juros = juros::Juros::novo(quantidade, composto, periodo, &pagamentos, &pesos);
     println!("Peso total = {}", juros.get_peso_total());
     println!("Acréscimo = {}", juros.juros_para_acrescimo(3.0));
     println!("Juros = {}", juros.acrescimo_para_juros(juros.juros_para_acrescimo(3.0), 15.0, 100, 50.0));

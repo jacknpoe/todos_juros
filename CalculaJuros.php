fc5305e7bc0326350f04514b7980eda2add3c64a
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2026-02-12 20:44:49 -0300

    Add files via upload
    tipagem de parâmetros e atributos, números floats, atualização e melhorias

--- CalculaJuros.php
+++ CalculaJuros.php
@@ -1,4 +1,6 @@
 <?php
+	declare(strict_types=1);
+
 	//***********************************************************************************************
 	// AUTOR: Ricardo Erick Rebêlo
 	// Objetivo: calcular os juros a partir das parcelas e as parcelas a partir dos juros (acréscimo)
@@ -9,6 +11,7 @@
 	// 1.0   20/04/2023 - Versão implantada compatível com a página de testes
 	// 1.1   26/10/2024 - adicionado botão "Valores Exemplo"
 	// 1.2   12/02/2026 - arrays e periodo como float
+	// 1.3   12/02/2026 - tipagem de parâmetros e atributos, números floats, atualização e melhorias
 
 	namespace jacknpoe;
 
@@ -17,22 +20,21 @@
 
 	class CalculaJuros
 	{
-		public $Quantidade = 0;
-		public $Composto = false;
-		public $Periodo = 30.0;
-		public $Pagamentos = array();
-		public $Pesos = array();
+		public int $Quantidade = 0;
+		public bool $Composto = false;
+		public float $Periodo = 30.0;
+		public array $Pagamentos = array();
+		public array $Pesos = array();
 
-		function __construct( $quantidade = 0, $composto = false, $periodo = 30)		// vamos ver se tem como sobrecarregar!
+		function __construct(int $quantidade = 0, bool $composto = false, float $periodo = 30.0)		// vamos ver se tem como sobrecarregar!
 		{
 			$this->Quantidade = $quantidade;
 			$this->Composto = $composto;
 			$this->Periodo = $periodo;
 		}
 
-		function setPagamentos( $delimitador = ",", $pagamentos = "")
+		function setPagamentos(string $delimitador = ",", $pagamentos = "")
 		{
-			$indice = 0;
 			if( $pagamentos === "")
 			{
 				for( $indice = 1; $indice <= $this->Quantidade; $indice++)
@@ -50,9 +52,8 @@
 			}
 		}
 
-		function setPesos( $delimitador = ",", $pesos = "")
+		function setPesos(string $delimitador = ",", $pesos = "")
 		{
-			$indice = 0;
 			if( $pesos === "")
 			{
 				for( $indice = 1; $indice <= $this->Quantidade; $indice++)
@@ -72,8 +73,7 @@
 
 		function getPesoTotal()
 		{
-			$acumulador = 0;
-			$indice = 0;
+			$acumulador = 0.0;
 
 			for( $indice = 1; $indice <= $this->Quantidade; $indice++)
 			{
@@ -82,54 +82,45 @@
 			return $acumulador;
 		}
 
-		function JurosParaAcrescimo( $juros)
+		function JurosParaAcrescimo(float $juros)
 		{
-			if( $juros <= 0 or $this->Quantidade <= 0 or $this->Periodo <= 0) return 0;
 			$total = $this->getPesoTotal();
-			$acumulador = 0;
-			// $soZero = true;
-			$indice = 0;
+			if( $juros <= 0.0 or $this->Quantidade < 1 or $this->Periodo <= 0.0 or $total <= 0.0) return 0.0;
+			$acumulador = 0.0;
 
 			for( $indice = 1; $indice <= $this->Quantidade; $indice++)
 			{
-				// if( $this->Pagamentos[ $indice] > 0 and $this->Pesos[ $indice] > 0 ) $soZero = false;
 
 				if( $this->Composto )
 				{	// COMPOSTO
-					$acumulador += $this->Pesos[ $indice] / pow( 1 + $juros / 100, $this->Pagamentos[ $indice] / $this->Periodo);
+					$acumulador += $this->Pesos[ $indice] / pow(1.0 + $juros / 100.0, $this->Pagamentos[ $indice] / $this->Periodo);
 				}
 				else
 				{	// SIMPLES
-					$acumulador += $this->Pesos[ $indice] / ( 1 + $juros / 100 * $this->Pagamentos[ $indice] / $this->Periodo);
+					$acumulador += $this->Pesos[ $indice] / (1.0 + $juros / 100.0 * $this->Pagamentos[ $indice] / $this->Periodo);
 				}
 			}
 
-			// if( $soZero ) return 0;
-			if( $acumulador <= 0) return 0;
-			return ( $total / $acumulador - 1) * 100;
+			if( $acumulador <= 0) return 0.0;
+			return ( $total / $acumulador - 1.0) * 100.0;
 		}
 
-		function AcrescimoParaJuros( $acrescimo, $precisao = 12, $maximoInteracoes = 100, $maximoJuros = 50, $acrescimoComoValorOriginal = false)
+		function AcrescimoParaJuros(float $acrescimo, int $precisao = 15, int $maximoInteracoes = 65, float $maximoJuros = 50.0, bool $acrescimoComoValorOriginal = false)
 		{
-			if( $maximoInteracoes < 1 or $this->Quantidade <= 0 or $precisao < 1 or $this->Periodo <= 0 or $acrescimo <= 0) return 0;
-			$minimoJuros = 0;
-			$medioJuros = 0;
-			$minimaDiferenca = 0;
-			$indice = 0;
 			$total = $this->getPesoTotal();
-			if( $total == 0) return 0;
+			if( $maximoInteracoes < 1 or $this->Quantidade < 1 or $precisao < 1 or $this->Periodo <= 0.0 or $acrescimo <= 0.0 or $total <= 0.0 or $maximoJuros <= 0.0) return 0.0;
+			$minimoJuros = 0.0;
+			$medioJuros = ( $minimoJuros + $maximoJuros) / 2.0;
+			$minimaDiferenca = pow( 0.1, $precisao);
 
 			if( $acrescimoComoValorOriginal )		// nesse caso, os pesos totais dão o valor cobrado e o acrescimo é o original
 			{
-				$acrescimo = 100 * ( $total / $acrescimo - 1);
-				if( $acrescimo <= 0) return 0;
+				$acrescimo = 100.0 * ( $total / $acrescimo - 1.0);
+				if( $acrescimo <= 0.0) return 0.0;
 			}
-
-			$minimaDiferenca = pow( 0.1, $precisao);
 
 			for( $indice = 1; $indice <= $maximoInteracoes; $indice++)
 			{
-				$medioJuros = ( $minimoJuros + $maximoJuros) / 2;
 				if( ( $maximoJuros - $minimoJuros ) < $minimaDiferenca ) break;
 				if( $this->JurosParaAcrescimo( $medioJuros ) <= $acrescimo )
 				{
@@ -139,6 +130,7 @@
 				{
 					$maximoJuros = $medioJuros;
 				}
+				$medioJuros = ( $minimoJuros + $maximoJuros) / 2.0;
 			}
 
 			return $medioJuros;

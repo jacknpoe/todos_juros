c51c0066723bb7f6fbdc1d70ec8d551cf1c036f8
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2026-02-16 10:56:30 -0300

    Add files via upload
    + alguns números forçados a serem double

--- juros.scm
+++ juros.scm
@@ -1,21 +1,21 @@
-;; C·lculo do juros, sendo que precisa de arrays pra isso
-;; Vers„o 0.1: 28/04/2024: vari·veis, getPesosTotal, jurosParaAcrescimo
+;; C√°lculo do juros, sendo que precisa de arrays pra isso
+;; Vers√£o 0.1: 28/04/2024: vari√°veis, getPesosTotal, jurosParaAcrescimo
 ;;        0.2: 29/04/2024: acrescimoParaJuros
-;;        0.3: 17/07/2024: corrige um erro em que multiplica por 10 em vez de 100 na linha 31 e n„o mostra pagamentos
+;;        0.3: 17/07/2024: corrige um erro em que multiplica por 10 em vez de 100 na linha 31 e n√£o mostra pagamentos
 
-;; Vari·veis
+;; VariÔøΩveis
 (define Quantidade 3)
 (define Composto 1)
 (define Periodo 30.0d0)
 (define Pagamentos '(30.0d0 60.0d0 90.0d0))
 (define Pesos '(1.0d0 1.0d0 1.0d0))
 
-;; calcula a somatÛria de Pesos[]
+;; calcula a somat√≥ria de Pesos[]
 (define (getPesoTotal)
   (_getPesoTotal(- Quantidade 1))
 )
 
-;; funÁ„o recursiva no lugar de um for com acumulador que realmente calcula a somatÛria de Pesos[]
+;; fun√ß√£o recursiva no lugar de um for com acumulador que realmente calcula a somat√≥ria de Pesos[]
 (define (_getPesoTotal valor)
   (if (= valor 0)
     (list-ref Pesos 0)
@@ -23,10 +23,10 @@
   )
 )
 
-;; calcula o acrÈscimo a partir dos juros e dados comuns (como parcelas)
+;; calcula o acr√©scimo a partir dos juros e dados comuns (como parcelas)
 (define (jurosParaAcrescimo juros)
-  (if (or (or (<= juros 0.0) (<= Quantidade 0)) (<= Periodo 0.0)) (return-from jurosParaAcrescimo 0.0))
-  (if (<= (getPesoTotal) 0.0) (return-from jurosParaAcrescimo 0.0))
+  (if (or (or (<= juros 0.0d0) (<= Quantidade 0)) (<= Periodo 0.0d0)) (return-from jurosParaAcrescimo 0.0d0))
+  (if (<= (getPesoTotal) 0.0d0) (return-from jurosParaAcrescimo 0.0d0))
   (if (= Composto 1)
       (* (- (/ (getPesoTotal) (_jurosCompostos (- Quantidade 1) juros)) 1.0d0) 100.0d0)
       (* (- (/ (getPesoTotal) (_jurosSimples (- Quantidade 1) juros)) 1.0d0) 100.0d0)
@@ -49,14 +49,14 @@
   )
 )
 
-;; calcula os juros a partir do acrÈscimo e dados comuns (como parcelas)
+;; calcula os juros a partir do acr√©scimo e dados comuns (como parcelas)
 (define (acrescimoParaJuros acrescimo precisao maxIteracoes maxJuros)
-  (if (or (or (or (or (or (<= acrescimo 0.0) (<= Quantidade 0)) (<= Periodo 0.0)) (< maxIteracoes 1)) (< precisao 1)) (<= maxJuros 0.0)) (return-from acrescimoParaJuros 0.0))
-  (if (<= (getPesoTotal) 0.0) (return-from acrescimoParaJuros 0.0))
+  (if (or (or (or (or (or (<= acrescimo 0.0d0) (<= Quantidade 0)) (<= Periodo 0.0d0)) (< maxIteracoes 1)) (< precisao 1)) (<= maxJuros 0.0d0)) (return-from acrescimoParaJuros 0.0d0))
+  (if (<= (getPesoTotal) 0.0d0) (return-from acrescimoParaJuros 0.0d0))
   (_acrescimoParaJuros acrescimo (expt 0.1d0 precisao) maxIteracoes 0.0d0 maxJuros (/ maxJuros 2.0d0))
 )
 
-;; funÁ„o recursiva no lugar de um for que realmente calcula o acrÈscimo
+;; fun√ß√£o recursiva no lugar de um for que realmente calcula o acr√©scimo
 (define (_acrescimoParaJuros acrescimo minDiferenca iteracaoAtual minJuros maxJuros medJuros)
   (if (or (= iteracaoAtual 0) (< (- maxJuros minJuros) minDiferenca))
     medJuros
@@ -71,7 +71,7 @@
 (display "Peso total = ")
 (display (getPesoTotal))
 (newline)
-(display "Acrescimo = ")
+(display "Acr√©scimo = ")
 (define acrescimo (jurosParaAcrescimo 3.0d0))
 (display acrescimo)
 (newline)

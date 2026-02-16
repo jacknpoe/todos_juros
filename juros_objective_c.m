2f78265626b4f585b13e5563100a07f695d946d6
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2026-02-16 10:31:08 -0300

    Add files via upload
    + 15 casas decimais na impressão agora no output e excluido potencia: expoente:, optado por po()

--- juros_objective_c.m
+++ juros_objective_c.m
@@ -1,5 +1,8 @@
 // Cálculo do juros, sendo que precisa de arrays pra isso
 // Versão 0.1: 27/03/2024: versão feita a partir de pesquisas no Google no jdoodle.com e POW com expoente inteiro
+//        0.2: 16/02/2026: 15 casas decimais na impressão e excluido potencia: expoente:, optado por po()
+// como está, roda em https://www.onlinegdb.com/online_objectivec_compiler e https://ideone.com/N7eo3l
+// JDoodle não linka libm (pow) / OneCompiler exige código ASCII puro (UTF-8 estrito), retire acento em "Acréscimo = "
 
 #import <Foundation/Foundation.h>
 #include <math.h> 
@@ -23,7 +26,6 @@
 - (void) setPeriodo: (double) p;
 - (void) putPagamento: (double) p;
 - (void) putPeso: (double) p;
-- (double) potencia: (double) valor expoente: (double) e;
 - (double) getPesoTotal;
 - (double) jurosParaAcrescimo: (double) juros;
 - (double) acrescimoParaJuros: (double) acrescimo precisao: (double) p maxIteracoes: (int) i maxJuros: (double) m;
@@ -65,22 +67,7 @@
 - (void) putPeso: (double) p {
     [Pesos addObject: [NSNumber numberWithDouble: p]];
 }
-// calcula a exponenciação do valor por e
-// o problema é que ela só funciona co expoentes inteiros
-// deve ser colocada uma exponenciação mais genérica
-// CORRIGIDO: como era erro do compilador, agora, com a correção, faz com ponto flutuante
-- (double) potencia: (double) valor expoente: (double) e {
-//    int expoente = (int) e;
-//    double acumulador = 1.0;
-//    int indice;
-//    for (indice = 0; indice < expoente; indice++) {
-//        acumulador *= valor;
-//    }
-//    return acumulador;
-//    return exp((float)e*log((float)valor));
-    return pow(valor, e);
-//    return (double) pow([NSNumber numberWithDouble: valor], [NSNumber numberWithDouble: e]);
-}
+
 // calcula a somatória de pesos
 - (double) getPesoTotal {
     double acumulador = 0;
@@ -90,28 +77,27 @@
     }
     return acumulador;
 }
+
 // calcula os juros a partir do acréscimo e dados comuns (como parcelas)
 - (double) jurosParaAcrescimo: (double) juros {
     if (juros <= 0.0 || Quantidade <= 0 || Periodo <= 0.0) return 0.0;
     double pesoTotal = [self getPesoTotal];
     if (pesoTotal <= 0.0) return 0.0;
     double acumulador = 0.0;
-    // BOOL soZero = YES;
     int indice;
 
     for (indice = 0; indice < Quantidade; indice++) {
-        // if([[Pagamentos objectAtIndex: indice] doubleValue] > 0.0 && [[Pesos objectAtIndex: indice] doubleValue] > 0.0) soZero = NO;
         if (Composto) {
             // ATENÇÃO: a potência só funciona com expoentes inteiros, porque a linguagem não tem POW!
-            acumulador += [[Pesos objectAtIndex: indice] doubleValue] / [self potencia: (1 + juros / 100) expoente: ([[Pagamentos objectAtIndex: indice] doubleValue] / Periodo)];
+            acumulador += [[Pesos objectAtIndex: indice] doubleValue] / pow(1 + juros / 100, [[Pagamentos objectAtIndex: indice] doubleValue] / Periodo);
         } else {
             acumulador += [[Pesos objectAtIndex: indice] doubleValue] / (1 + juros / 100 * [[Pagamentos objectAtIndex: indice] doubleValue] / Periodo);
         }
     }
-    // if (soZero) return 0.0;
     if (acumulador <= 0.0) return 0.0;
     return (pesoTotal / acumulador - 1) * 100;
 }
+
 // calcula o acréscimo a partir dos juros e dados comuns (como parcelas)
 - (double) acrescimoParaJuros: (double) acrescimo precisao: (double) p maxIteracoes: (int) mi maxJuros: (double) mj {
     if (mi < 1 || Quantidade < 1 || p < 1 || Periodo <= 0.0 || acrescimo <- 0.0 || mj <= 0.0 ) return 0.0;
@@ -120,7 +106,7 @@
     double minJuros = 0.0;
     double medJuros = mj / 2.0;
     double maxJuros = mj;
-    double minDiferenca = [self potencia: 0.1 expoente: p];
+    double minDiferenca = pow(0.1, p);
     int indice;
     
     for (indice = 0; indice < mi; indice++) {
@@ -135,6 +121,8 @@
     
     return medJuros;
 }
+
+// desaloca arrays
 - (void) desaloca {
     [Pagamentos release];
     [Pesos release];
@@ -163,12 +151,12 @@ int main (int argc, const char * argv[])
     double acrescimo = [juros jurosParaAcrescimo: 3.0];
     double juroscalc = [juros acrescimoParaJuros: acrescimo precisao: 15.0 maxIteracoes: 100 maxJuros: 50.0];
 
-    NSLog(@"Peso Total = %f", pesoTotal);
-    NSLog(@"Acréscimo = %f", acrescimo);
-    NSLog(@"Juros = %f", juroscalc);
-    // NSLog(@"Pow = %f", pow(1.03, 0.5));
-    // NSLog(@"Pow = %f", pow(acrescimo, [juros Pagamento: 1] / [juros Periodo]));
-
+    // impressão
+    printf("Peso Total = %.15f\n", pesoTotal);
+    printf("Acréscimo = %.15f\n", acrescimo);
+    printf("Juros = %.15f\n", juroscalc); 
+ 
+    // desalocação
     [juros desaloca];
     [juros release];
     [pool drain];

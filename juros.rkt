238249b3c3d88410c05bac2c3b78ac07dffdbd60
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2026-04-17 12:30:07 -0300

    Add files via upload
    corrigida pelo ChatGPT, tinham números com d0 no final e getPesoTotal funcionava como variável;
    troca de vetores estáticos para listas dinâmicas (ChatGPT), reorganização obrigatória de defines
    troca de list-ref para car e cdr nas recursões que percorrem as listas

--- juros.rkt
+++ juros.rkt
@@ -1,51 +1,84 @@
 #lang racket
 
-;; Cálculo do juros, sendo que precisa de vetores para isso
+;; Cálculo do juros, sendo que precisa de listas para isso
 ;; Versão 0.1: 20/05/2024: completa, começando do zero, usando o Google
+;;        0.2: 17/06/2026: corrigida pelo ChatGPT, tinham números com d0 no final e getPesoTotal funcionava como variável;
+;;                         troca de vetores estáticos para listas dinâmicas (ChatGPT), reorganização obrigatória de defines
+;;                         troca de list-ref para car e cdr nas recursões que percorrem as listas
 
-;; dados gerais
+;; dados gerais escalares
 (define Quantidade 3)
 (define Composto 1)  ;; 1 = true, 0 = false
 (define Periodo 30.0)
-(define Pagamentos (vector 30.0 60.0 90.0))
-(define Pesos (vector 1.0 1.0 1.0))
+
+;; função recursiva que realmente monta Pagamentos[]
+(define (rCriaPagamentos indice)
+  (if (< indice 0)
+    '()
+    (cons (* (- Quantidade indice) Periodo)
+          (rCriaPagamentos (- indice 1)))
+  )
+)
+
+;; função açúcar que monta Pagamentos[]
+(define (criaPagamentos)
+  (rCriaPagamentos (- Quantidade 1))
+)
+
+;; função recursiva que realmente monta Pesos[]
+(define (rCriaPesos indice)
+  (if (< indice 0)
+    '()
+    (cons 1.0
+          (rCriaPesos (- indice 1)))
+  )
+)
+
+;; função açúcar que monta Pesos[]
+(define (criaPesos)
+  (rCriaPesos (- Quantidade 1))
+)
+
+;; dados gerais listas
+(define Pagamentos (criaPagamentos))
+(define Pesos (criaPesos))
 
 ;; função recursiva que realmente calcula a somatória de pesos[]
-(define (rGetPesoTotal indice)
-  (if (= indice 0)
-    (vector-ref Pesos 0)
-    (+ (vector-ref Pesos indice) (rGetPesoTotal (- indice 1)))
+(define (rGetPesoTotal lista)
+  (if (null? lista)
+    0.0
+    (+ (car lista) (rGetPesoTotal (cdr lista)))
   )
 )
 
-;; perfume que chama a função recursiva que calcula a somatória de pesos[]
-(define getPesoTotal
-  (rGetPesoTotal (- Quantidade 1))
+;; função açúcar que chama a função recursiva que calcula a somatória de pesos[]
+(define (getPesoTotal)
+  (rGetPesoTotal Pesos)
 )
 
-;; calcula a soma do amortecimento de todas as parcelas para juros compostos
-(define (rJurosCompostos juros indice)
-  (if (= indice 0)
-    (/ (vector-ref Pesos 0) (expt (+ 1.0 (/ juros 100.0)) (/ (vector-ref Pagamentos 0) Periodo)))
-    (+ (/ (vector-ref Pesos indice) (expt (+ 1.0d0 (/ juros 100.0d0)) (/ (vector-ref Pagamentos indice) Periodo))) (rJurosCompostos juros (- indice 1)))
+;; função recursiva que calcula a soma do amortecimento de todas as parcelas para juros compostos
+(define (rJurosCompostos juros pagamentos pesos)
+  (if (null? pagamentos)
+    0.0
+    (+ (/ (car pesos) (expt (+ 1.0 (/ juros 100.0)) (/ (car pagamentos) Periodo))) (rJurosCompostos juros (cdr pagamentos) (cdr pesos)))
   )
 )
 
-;; calcula a soma do amortecimento de todas as parcelas para juros simples
-(define (rJurosSimples juros indice)
-  (if (= indice 0)
-    (/ (vector-ref Pesos 0) (+ 1.0 (* (/ juros 100.0) (/ (vector-ref Pagamentos 0) Periodo))))
-    (+ (/ (vector-ref Pesos indice) (+ 1.0 (* (/ juros 100.0) (/ (vector-ref Pagamentos indice) Periodo)))) (rJurosSimples juros (- indice 1)))
-  )    
+;; função recursiva que calcula a soma do amortecimento de todas as parcelas para juros simples
+(define (rJurosSimples juros pagamentos pesos)
+  (if (null? pagamentos)
+    0.0
+    (+ (/ (car pesos) (+ 1.0 (* (/ juros 100.0) (/ (car pagamentos) Periodo)))) (rJurosSimples juros (cdr pagamentos) (cdr pesos)))
+  )
 )
 
-;; perfume que calcula o acréscimo a partir dos juros e dados comuns (como parcelas)
+;; função açúcar que calcula o acréscimo a partir dos juros e dados comuns (como parcelas)
 (define (jurosParaAcrescimo juros)
-  (if (or (<= juros 0.0) (< Quantidade 1) (<= Periodo 0.0) (<= getPesoTotal 0.0))
+  (if (or (<= juros 0.0) (< Quantidade 1) (<= Periodo 0.0) (<= (getPesoTotal) 0.0))
     0.0
     (if (= Composto 1)
-      (* (- (/ getPesoTotal (rJurosCompostos juros (- Quantidade 1))) 1.0) 100.0)
-      (* (- (/ getPesoTotal (rJurosSimples juros (- Quantidade 1))) 1.0) 100.0)
+      (* (- (/ (getPesoTotal) (rJurosCompostos juros Pagamentos Pesos)) 1.0) 100.0)
+      (* (- (/ (getPesoTotal) (rJurosSimples juros Pagamentos Pesos)) 1.0) 100.0)
     )
   )
 )
@@ -61,9 +94,9 @@
   )
 )
 
-;; perfume que calcula os juros a partir do acréscimo e dados comuns (como parcelas)
+;; função açúcar que calcula os juros a partir do acréscimo e dados comuns (como parcelas)
 (define (acrescimoParaJuros acrescimo precisao maxIteracoes maxJuros)
-  (if (or (<= acrescimo 0.0) (< Quantidade 1) (<= Periodo 0.0) (< maxIteracoes 1) (< precisao 1) (<= maxJuros 0.0) (<= getPesoTotal 0.0))
+  (if (or (<= acrescimo 0.0) (< Quantidade 1) (<= Periodo 0.0) (< maxIteracoes 1) (< precisao 1) (<= maxJuros 0.0) (<= (getPesoTotal) 0.0))
     0.0
     (rAcrescimoParaJuros acrescimo (expt 0.1 precisao) maxIteracoes 0.0 maxJuros (/ maxJuros 2.0))
   )
@@ -71,11 +104,11 @@
 
 ;; testa as funções
 (display "Peso total = ")
-(display getPesoTotal)
+(display (getPesoTotal))
 (display "\n")
 (display "Acréscimo = ")
 (display (jurosParaAcrescimo 3.0))
 (display "\n")
 (display "Juros = ")
-(display (acrescimoParaJuros (jurosParaAcrescimo 3.0) 15 100 50.0))
+(display (acrescimoParaJuros (jurosParaAcrescimo 3.0) 15 65 50.0))
 (display "\n")

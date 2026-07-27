cd67976253c90b6fee852c0083475b52558338d0
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2026-07-27 12:53:32 -0300

    Add files via upload
    melhorias nos comentários

--- juros_otimizado.c
+++ juros_otimizado.c
@@ -2,7 +2,7 @@
 // Versão 0.1: 15/04/2026: versão pesadamente e estaticamente otimizada copiada da versão normal em C
 
 // OBJETIVO: Esta versão altamente otimizada existe para estudar otimizações de desempenho sobre a implementação
-//           canônica, preservando os resultados, mas não necessariamente os mesmos algoritmos.
+//           canônica, preservando os resultados, mas não necessariamente os algoritmos.
 //           O foco é desempenho, mesmo que isso implique sacrificar legibilidade, reutilização ou semelhança com a 
 //           implementação canônica (juros.c). Ela não foi pensada como material didático.
 //           Também não deve ser vista como representante de C no repositório https://github.com/jacknpoe/todos_juros/

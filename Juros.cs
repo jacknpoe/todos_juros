4938f320a6ecf045fcfca4d1091ce6741cb2ab3a
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2026-08-09 08:26:25 -0300

    Add files via upload

--- Juros.cs
+++ Juros.cs
@@ -27,45 +27,42 @@ public class Juros
     }
 
     public double getPesoTotal(){
-        double acumulador = 0;
+        double acumulador = 0.0;
         for(int indice = 0; indice < Quantidade; indice++) acumulador += Pesos[indice];
         return acumulador;
     }
 
     public double jurosParaAcrescimo(double juros) {
-        if(juros == 0 || Quantidade == 0 || Periodo <= 0.0) return 0;
+        if(juros <= 0.0 || Quantidade == 0 || Periodo <= 0.0) return 0.0;
         double pesoTotal = getPesoTotal();
-        if(pesoTotal == 0) return 0;
-        double acumulador = 0;
-        // bool soZero = true;
+        if(pesoTotal == 0) return 0.0;
+        double acumulador = 0.0;
 
         for(int indice = 0; indice < Quantidade; indice ++) {
-            // if(Pagamentos[indice] > 0.0 && Pesos[indice] > 0) soZero = false;
             if(Composto) {
-                acumulador += Pesos[indice] / Math.Pow(1 + juros / 100, Pagamentos[indice] / Periodo);
+                acumulador += Pesos[indice] / Math.Pow(1.0 + juros / 100.0, Pagamentos[indice] / Periodo);
             } else {
-                acumulador += Pesos[indice] / (1 + juros / 100 * Pagamentos[indice] / Periodo);
+                acumulador += Pesos[indice] / (1.0 + juros / 100.0 * Pagamentos[indice] / Periodo);
             }
         }
 
-        // if(soZero) return 0;
-        if (acumulador <= 0 ) return 0;
-        return (pesoTotal / acumulador - 1) * 100;
+        if (acumulador <= 0 ) return 0.0;
+        return (pesoTotal / acumulador - 1.0) * 100.0;
     }
 
-    public double acrescimoParaJuros(double acrescimo, int precisao = 15, int maxInteracoes = 65, double maxJuros = 50) {
-        if(maxInteracoes < 1 || Quantidade == 0 || precisao < 1 || Periodo <= 0.0 || acrescimo <= 0 || maxJuros <= 0) return 0;
-        double minJuros = 0, medJuros = 0, minDiferenca = Math.Pow(0.1, precisao), pesoTotal = getPesoTotal();
-        if(pesoTotal == 0) return 0;
+    public double acrescimoParaJuros(double acrescimo, int precisao = 15, int maxInteracoes = 65, double maxJuros = 50.0) {
+        if(maxInteracoes < 1 || Quantidade == 0 || precisao < 1 || Periodo <= 0.0 || acrescimo <= 0.0 || maxJuros <= 0.0) return 0.0;
+        double minJuros = 0.0, medJuros = (minJuros + maxJuros) / 2.0, minDiferenca = Math.Pow(0.1, precisao), pesoTotal = getPesoTotal();
+        if(pesoTotal == 0) return 0.0;
 
         for(int indice = 0; indice < maxInteracoes; indice++) {
-            medJuros = (minJuros + maxJuros) / 2;
             if((maxJuros - minJuros) < minDiferenca) break;
             if(jurosParaAcrescimo(medJuros) <= acrescimo){
                 minJuros = medJuros; 
             } else {
                 maxJuros = medJuros;
             }
+            medJuros = (minJuros + maxJuros) / 2.0;
         }
 
         return medJuros;
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+// using Juros;
+
+class TesteJuros {
+    static void Main()
+    {
+        Juros juros = new Juros(3, true, 30.0);
+
+        for(int i = 0; i < juros.Quantidade; i++) {
+            juros.Pagamentos[i] = juros.Periodo * (i + 1);
+            juros.Pesos[i] = 1.0;
+        }
+
+        double pesoTotal = juros.getPesoTotal();
+        double acrescimoCalculado = juros.jurosParaAcrescimo(3.0);
+        double jurosCalculado = juros.acrescimoParaJuros(acrescimoCalculado);
+
+        System.Console.Write("Peso total = ");
+        System.Console.WriteLine(pesoTotal);
+        System.Console.Write("Acréscimo = ");
+        System.Console.WriteLine(acrescimoCalculado);
+        System.Console.Write("Juros = ");
+        System.Console.WriteLine(jurosCalculado);
+    }
+}

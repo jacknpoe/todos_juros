4f548a986ee99e4da9112971479514326484fc11
Ricardo Erick Rebêlo <jacknpoe@yahoo.com>
2026-08-09 08:19:03 -0300

    Add files via upload

--- Program.vb
+++ Program.vb
@@ -1,94 +1,120 @@
-﻿Module Program
+' Cálculo do juros, sendo que precisa de arrays pra isso
+' Versão 0.1: 02/03/2024: versão feita sem muito conhecimento de Visual Basic .NET
+'        0.2: 04/04/2024: mudada a lógica de soZero para acumulador <= 0.0
+'        0.3: 09/08/2026: feito projeto, comentários, tornada escalável
 
-  Public Class Juros
+' Classe juros com atributos para simplificar as chamadas aos métodos
+Public Class Juros
     Private PQuantidade As Integer
     Public Composto As Boolean
     Public Periodo as Double
     Public Pagamentos() as Double
     Public Pesos() As Double
 
+    ' a propriedade Quantidade deve alterar os tamanhos dos arrays
     Public Property Quantidade() As Integer
-      Get
-        Return PQuantidade
-      End Get
-      Set(ByVal Valor As Integer)
-        PQuantidade = Valor
-        ReDim Preserve Pagamentos(Valor-1)
-        ReDim Preserve Pesos(Valor-1)
-      End Set
+        Get
+            Return PQuantidade
+        End Get
+        Set(ByVal Valor As Integer)
+            PQuantidade = Valor
+            ReDim Preserve Pagamentos(Valor-1)
+            ReDim Preserve Pesos(Valor-1)
+        End Set
     End Property
 
+    ' construtor que inicializa atributos escalares
     Sub New(ByVal quant As Integer, ByVal comp As Boolean, ByVal period As Double)
-      Quantidade = quant
-      Composto = comp
-      Periodo = period
+        Quantidade = quant
+        Composto = comp
+        Periodo = period
     End Sub
 
+    ' calcula a somatória dos elementos no array Pesos
     Public Function GetPesoTotal As Double
-      Dim Acumulador As Double = 0.0
-      For Indice As Integer = 0 To Quantidade - 1
-        Acumulador = Acumulador + Pesos(Indice)
-      Next Indice
-      Return Acumulador
+        Dim Acumulador As Double = 0.0
+        For Indice As Integer = 0 to Quantidade - 1
+            Acumulador = Acumulador + Pesos(Indice)
+        Next Indice
+        Return Acumulador
     End Function
 
+    ' calcula o acréscimo a partir dos juros e parcelas
     Public Function JurosParaAcrescimo(ByVal juros As Double) As Double
-      Dim pesoTotal As Double
-      Dim acumulador As Double = 0.0
-      ' Dim soZero As Boolean = True
-      If juros <= 0.0 Or Quantidade <= 0 Or Periodo <= 0.0 Then Return 0.0
-      pesoTotal = GetPesoTotal
-      If pesoTotal <= 0.0 Then Return 0.0
+        Dim pesoTotal As Double
+        Dim acumulador As Double = 0.0
+        If juros <= 0.0 Or Quantidade <= 0 Or Periodo <= 0.0 Then Return 0.0
+        pesoTotal = GetPesoTotal
+        If pesoTotal <= 0.0 Then Return 0.0
 
-      For Indice As Integer = 0 To Quantidade - 1
-          ' If Pagamentos(Indice) > 0.0 And Pesos(Indice) > 0.0 Then soZero = False
-        If Composto Then
-          acumulador = acumulador + Pesos(Indice) / (1 + juros / 100) ^ (Pagamentos(Indice) / Periodo)
-        Else
-          acumulador = acumulador + Pesos(Indice) / (1 + juros / 100 * Pagamentos(Indice) / Periodo)
-        End If
-      Next Indice
+        For Indice As Integer = 0 to Quantidade - 1
+            If Composto Then
+                acumulador = acumulador + Pesos(Indice) / (1 + juros / 100) ^ (Pagamentos(Indice) / Periodo)
+            Else
+                acumulador = acumulador + Pesos(Indice) / (1 + juros / 100 * Pagamentos(Indice) / Periodo)
+            End If
+        Next Indice
 
-      ' If soZero Then Return 0.0
-      If acumulador <= 0.0 Then Return 0.0
-      Return (pesoTotal / acumulador - 1) * 100
+        If acumulador <= 0.0 Then Return 0.0
+        Return (pesoTotal / acumulador - 1) * 100
     End Function
 
+    ' calcula os juros a partir do acréscimo e parcelas
     Public Function AcrescimoParaJuros(ByVal acrescimo As Double, ByVal precisao As Integer, ByVal maxIteracoes As Integer, ByVal maxJuros As Double) As Double
-      Dim minJuros As Double = 0.0
-      Dim medJuros As Double
-      Dim minDiferenca As Double
-      Dim PesoTotal As Double
-      If maxIteracoes < 1 Or Quantidade < 1 Or precisao < 1 Or Periodo <= 0.0 Or acrescimo <= 0 Or maxJuros <= 0 Then Return 0.0
-      PesoTotal = GetPesoTotal
-      If PesoTotal <= 0.0 Then Return 0.0
-      minDiferenca = 0.1 ^ precisao
+        Dim minJuros As Double = 0.0
+        Dim medJuros As Double
+        Dim minDiferenca As Double
+        Dim PesoTotal As Double
+        If maxIteracoes < 1 Or Quantidade < 1 Or precisao < 1 Or Periodo <= 0.0 Or acrescimo <= 0 Or maxJuros <= 0 Then Return 0.0
+        PesoTotal = GetPesoTotal()
+        If PesoTotal <= 0.0 Then Return 0.0
+        minDiferenca = 0.1 ^ precisao
 
-      For Indice As Integer = 0 To maxIteracoes - 1
-        medJuros = (minJuros + maxJuros) / 2
-        If (maxJuros - minJuros) < minDiferenca Then Return medJuros
-        if JurosParaAcrescimo(medJuros) <= acrescimo Then
-          minJuros = medJuros
-        Else
-          maxJuros = medJuros
-        End If
-      Next Indice
+        For Indice As Integer = 0 To maxIteracoes - 1
+            medJuros = (minJuros + maxJuros) / 2
+            If (maxJuros - minJuros) < minDiferenca Then Return medJuros
+            If JurosParaAcrescimo(medJuros) <= acrescimo Then
+                minJuros = medJuros
+            Else
+                maxJuros = medJuros
+            End If
+        Next Indice
 
-      Return medJuros
+        Return medJuros
     End Function
-  End Class
+End Class
 
-  Sub Main(args as String())
-    Dim meuJuros As Juros = New Juros(3, True, 30.0)
+' programa de testes
+Public Class Program
+    ' método de testes
+    Public Shared Sub Main()
+        ' variáveis para configurar os atributos escalares
+        Dim Quantidade As Integer = 3
+        Dim Composto As Boolean = true
+        Dim Periodo As Double = 30.0
 
-    For Indice As Integer = 0 To 2
-      meuJuros.Pagamentos(Indice) = 30.0 * (Indice + 1.0)
-      meuJuros.Pesos(Indice) = 1.0
-    Next Indice
+        ' variáveis que guardam os retornos dos métodos
+        Dim pesoTotal As Double
+        Dim acrescimoCalculado As Double
+        Dim jurosCalculado as Double
 
-    Console.WriteLine("Peso total = {0}", meuJuros.GetPesoTotal)
-    Console.WriteLine("Acrescimo = {0}", meuJuros.JurosParaAcrescimo(3.0))
-    Console.WriteLine("Juros = {0}", meuJuros.AcrescimoParaJuros(meuJuros.JurosParaAcrescimo(3.0), 15, 100, 50.0))
-  End Sub
+        ' objeto meuJuros do tipo Juros, inicializa escalares
+        Dim meuJuros As Juros = New Juros(Quantidade, Composto, Periodo)
 
-End Module
+        ' inicializa os elementos dos arrays Pagamentos e Pesos
+        For Indice As Integer = 0 To Quantidade - 1
+            meuJuros.Pagamentos(Indice) = (Indice + 1) * Periodo
+            meuJuros.Pesos(Indice) = 1.0
+        Next Indice
+
+        ' calcula e guarda os resultados
+        pesoTotal = meuJuros.GetPesoTotal()
+        acrescimoCalculado = meuJuros.JurosParaAcrescimo(3.0)
+        jurosCalculado = meuJuros.AcrescimoParaJuros(acrescimoCalculado, 15, 65, 50.0)
+
+        ' imprime os resultados
+        Console.WriteLine("Peso total = " & pesoTotal)
+        Console.WriteLine("Acréscimo = " & acrescimoCalculado)
+        Console.WriteLine("Juros = " & jurosCalculado)
+    End Sub
+End Class
